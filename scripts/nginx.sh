3b4acb3f3cf20c84720c874e7997fb2320f3c34e
felix ayange <felixdecoder2020@gmail.com>
2023-06-05 22:31:58 +0100

    fix link exist issue

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,13 +1,10 @@
-
 #!/usr/bin/bash
 
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/django-blog-project/nginx/nginx.conf /etc/nginx/sites-available/blog
+sudo rm -f /etc/nginx/sites-enabled/blog  # Remove existing link
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-#sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
-
